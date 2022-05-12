42577a5091a7a498b7ffc50d6a6b9b30af856c94
sha112419 <104012644+sha112419@users.noreply.github.com>
2022-05-12 14:25:55 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+package com.sample.projecttest;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[]name = {11,19,24,2,6,11,24};
+		System.out.println(name[5]);
+		System.out.println(name[6]);
+		int[]name1 = new int[3];
+		name1[0] = 10;
+		name1[1] = 20;
+		name1[2] = 30;
+		
+		System.out.println(name1[1]);
+		
+		String[]name2 = {"sha","rah","mec","it"};
+		System.out.println(name2[3]);
+
+	}
+
+}
